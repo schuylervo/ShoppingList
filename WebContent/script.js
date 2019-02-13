40c92b921092b12fa6fdbd1875c140697563cce2
Schuyler <svonoeyen@gmail.com>
2019-02-12 23:59:27 -0500

    third commit

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,6 +1,7 @@
 
 var items = [];
 var prices = [];
+var sum = 0;
 
 function addToCart(item, price) {
 	
@@ -8,21 +9,33 @@ function addToCart(item, price) {
 		prices.push(price);
 	 
 	}
+
+function calcSalesTax (sum) {
+	var salesTax = 1.06* sum;
+	return salesTax;
+}
+
+function calcGrandTotal (sum) {
+	var grandTotal = 1.06* sum + sum;
+	return grandTotal;
+}
 	
 
 function checkOut() {
 	
-	var sum =0;
-	
 	var nameEl = document.getElementById("tablehead");
 	
 	var checkEl = document.getElementById("checkouttablehead");
+	var taxEl = document.getElementById("tax");
+	var grandEl = document.getElementById("grand");
 	
 	for (var j = 0; j<prices.length; j++) {
 		sum += prices[j];	
 	}
 	
-	checkEl.insertAdjacentHTML("beforeend", "<tr id = 'checkouttablehead'><td> </td><td> $"+ Number(sum).toFixed(2) +" </td></tr>");	
+	checkEl.insertAdjacentHTML("beforeend", "<tr id = 'checkouttablehead'><td> </td><td> $"+ Number(sum).toFixed(2) +" </td></tr>");
+	taxEl.insertAdjacentHTML("beforeend", "<tr id = 'tax'><td> </td><td> $"+ Number(sum*1.06 - sum).toFixed(2) +" </td></tr>");	
+	grandEl.insertAdjacentHTML("beforeend", "<tr id = 'grand'><td> </td><td> $"+ Number(sum*1.06).toFixed(2) +" </td></tr>");
 	
 	for (var i=0; i<items.length; i++) {
 		
